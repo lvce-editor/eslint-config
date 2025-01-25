99cd918bac0d999804f5d94dc5c640f23b736bed
Le Vivilet <levivlet@pm.me>
2025-01-25 23:41:57 +0100

    feature: update plugin structure

--- packages/rules/src/index.ts
+++ packages/rules/src/index.ts
@@ -3,23 +3,26 @@ import json from '@eslint/json'
 
 const plugin = {
   meta: {
-    name: '@lvce-editor/tsconfig',
-    version: '0.0.1', // x-release-please-version
+    name: 'tsconfig',
+    // version: '0.0.1', // x-release-please-version
   },
+  files: ['**/tsconfig.json'],
   languages: {
     ...json.languages,
   },
   rules: {
-    'tsconfig/no-unchecked-side-effect-imports': tsconfigNoUncheckedSideEffectImports,
+    'no-unchecked-side-effect-imports': tsconfigNoUncheckedSideEffectImports,
   },
-  configs: {
-    recommended: {
-      plugins: {},
-      rules: /** @type {const} */ {
-        'tsconfig/no-unchecked-side-effect-imports': 'error',
-      },
-    },
+  configs: {},
+}
+
+const recommended = {
+  plugins: {
+    tsconfig: plugin,
+  },
+  rules: /** @type {const} */ {
+    'tsconfig/no-unchecked-side-effect-imports': 'error',
   },
 }
 
-export default plugin
+export default recommended
--- packages/rules/src/tsconfig-no-unchecked-side-effect-imports.ts
+++ packages/rules/src/tsconfig-no-unchecked-side-effect-imports.ts
@@ -1,41 +1,37 @@
-export default {
-  meta: {
-    type: 'problem' as const,
-
-    docs: {
-      description: 'Disallow not allowed keys in JSON objects',
-    },
-
-    messages: {
-      noUncheckedSideEffectImport: 'noUncheckedSideEffectImport rule should be enabled',
-    },
+export const meta = {
+  type: 'problem' as const,
+  docs: {
+    description: 'Disallow not allowed keys in JSON objects',
   },
+  messages: {
+    noUncheckedSideEffectImport: 'noUncheckedSideEffectImport rule should be enabled',
+  },
+}
 
-  create(context: any) {
-    return {
-      Member(node: any) {
-        if (node.name.type === 'String' && node.name.value === 'compilerOptions' && node.value.type === 'Object') {
-          const members = node.value.members
-          for (const member of members) {
-            if (member.name.type === 'String' && member.name.value === 'noUncheckedSideEffectImports') {
-              if (member.value.value === true) {
-                return
-              }
-              if (member.value.value === false) {
-                context.report({
-                  loc: member.name.loc,
-                  messageId: 'noUncheckedSideEffectImport',
-                })
-              }
+export const create = (context: any) => {
+  return {
+    Member(node: any) {
+      if (node.name.type === 'String' && node.name.value === 'compilerOptions' && node.value.type === 'Object') {
+        const members = node.value.members
+        for (const member of members) {
+          if (member.name.type === 'String' && member.name.value === 'noUncheckedSideEffectImports') {
+            if (member.value.value === true) {
               return
             }
+            if (member.value.value === false) {
+              context.report({
+                loc: member.name.loc,
+                messageId: 'noUncheckedSideEffectImport',
+              })
+            }
+            return
           }
-          context.report({
-            loc: node.name.loc,
-            messageId: 'noUncheckedSideEffectImport',
-          })
         }
-      },
-    }
-  },
+        context.report({
+          loc: node.name.loc,
+          messageId: 'noUncheckedSideEffectImport',
+        })
+      }
+    },
+  }
 }
