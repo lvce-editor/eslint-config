5254d6a8a967560086f77ea325c349afb60a2194
Le Vivilet <levivlet@pm.me>
2025-01-25 23:56:57 +0100

    remove log

--- packages/e2e/test/sample-tsconfig-no-side-effect-imports.test.ts
+++ packages/e2e/test/sample-tsconfig-no-side-effect-imports.test.ts
@@ -3,6 +3,5 @@ import { runFixture } from './util.ts'
 
 test('sample-tsconfig-no-side-effect-imports', async () => {
   const { parsed, expected } = await runFixture('sample-tsconfig-no-side-effect-imports')
-  console.log({ parsed, expected })
   expect(parsed).toEqual(expected)
 })
