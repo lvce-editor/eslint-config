9d4dd531eb34e213d71a41c8ce4893200178b508
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-26 00:26:07 +0100

    feature: simplify duplicate rules code (#71)
    * feature: simplify duplicate rules code
    
    * name
    
    * fix
    
    * fix

--- packages/rules/src/tsconfig-force-consistent-casing-in-file-names.ts
+++ packages/rules/src/tsconfig-force-consistent-casing-in-file-names.ts
@@ -1,3 +1,5 @@
+import * as tsConfigCompilerOption from './tsconfig-compiler-option.ts'
+
 export const meta = {
   type: 'problem' as const,
   docs: {
@@ -9,29 +11,5 @@ export const meta = {
 }
 
 export const create = (context: any) => {
-  return {
-    Member(node: any) {
-      if (node.name.type === 'String' && node.name.value === 'compilerOptions' && node.value.type === 'Object') {
-        const members = node.value.members
-        for (const member of members) {
-          if (member.name.type === 'String' && member.name.value === 'forceConsistentCasingInFileNames') {
-            if (member.value.value === true) {
-              return
-            }
-            if (member.value.value === false) {
-              context.report({
-                loc: member.name.loc,
-                messageId: 'forceConsistentCasingInFileNames',
-              })
-            }
-            return
-          }
-        }
-        context.report({
-          loc: node.name.loc,
-          messageId: 'forceConsistentCasingInFileNames',
-        })
-      }
-    },
-  }
+  return tsConfigCompilerOption.create(context, 'forceConsistentCasingInFileNames')
 }
