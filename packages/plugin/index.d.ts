0c78aaf4735936fbe42af58a36d3648fc1a08ada
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-25 23:53:17 +0100

    feature: update config types (#64)

--- packages/plugin/index.d.ts
+++ packages/plugin/index.d.ts
@@ -1,8 +1,10 @@
 declare const config: readonly any[] & {}
 declare const recommendedNode: readonly any[] & {}
 declare const recommendedFolderStructure: readonly any[] & {}
+declare const recommendedTsconfig: readonly any[] & {}
 
 export { recommendedNode }
 export { recommendedFolderStructure }
+export { recommendedTsconfig }
 
 export default config
