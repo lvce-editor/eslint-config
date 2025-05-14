fc097913b09d953364d5af292a157f11aab158f5
Le Vivilet <levivlet@pm.me>
2025-05-14 20:15:39 +0200

    feature: move built packages into separate folders

--- packages/build/src/bundleJs.js
+++ packages/build/src/bundleJs.js
@@ -1,41 +1,39 @@
 import pluginTypeScript from '@babel/preset-typescript'
 import { babel } from '@rollup/plugin-babel'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { join } from 'path'
 import { rollup } from 'rollup'
-import { root } from './root.js'
 
-/**
- * @type {import('rollup').RollupOptions}
- */
-const options = {
-  input: join(root, 'packages/plugin-github-actions/src/index.ts'),
-  preserveEntrySignatures: 'strict',
-  treeshake: {
-    propertyReadSideEffects: false,
-  },
-  output: {
-    file: join(root, '.tmp/plugin-github-actions/dist/index.js'),
-    format: 'es',
-    freeze: false,
-    generatedCode: {
-      constBindings: true,
-      objectShorthand: true,
+export const bundleJs = async ({ inFile, outFile }) => {
+  /**
+   * @type {import('rollup').RollupOptions}
+   */
+  const options = {
+    input: inFile,
+    preserveEntrySignatures: 'strict',
+    treeshake: {
+      propertyReadSideEffects: false,
     },
-    inlineDynamicImports: true,
-  },
-  external: ['@lvce-editor/ripgrep', 'electron', 'execa', 'ws', 'yaml-eslint-parser', 'eslint-compat-utils', 'eslint-plugin-yml'],
-  plugins: [
-    babel({
-      babelHelpers: 'bundled',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      presets: [pluginTypeScript],
-    }),
-    nodeResolve(),
-  ],
-}
+    output: {
+      file: outFile,
+      format: 'es',
+      freeze: false,
+      generatedCode: {
+        constBindings: true,
+        objectShorthand: true,
+      },
+      inlineDynamicImports: true,
+    },
+    external: ['@lvce-editor/ripgrep', 'electron', 'execa', 'ws', 'yaml-eslint-parser', 'eslint-compat-utils', 'eslint-plugin-yml', '@eslint/json'],
+    plugins: [
+      babel({
+        babelHelpers: 'bundled',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        presets: [pluginTypeScript],
+      }),
+      nodeResolve(),
+    ],
+  }
 
-export const bundleJs = async () => {
   const input = await rollup(options)
   // @ts-ignore
   await input.write(options.output)
