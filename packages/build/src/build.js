d9ff7119ad0274cafc37b19d114ed641b2ed1da7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-14 20:18:15 +0200

    feature: move built packages into separate folders (#164)

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -2,8 +2,10 @@ import { execa } from 'execa'
 import { cp, mkdir, readFile, rm, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { root } from './root.js'
+import { bundleJs } from './bundleJs.js'
 
-const dist = join(root, 'dist')
+const tmp = join(root, '.tmp')
+const dist = join(root, '.tmp', 'dist')
 
 const readJson = async (path) => {
   const content = await readFile(path, 'utf8')
@@ -52,7 +54,7 @@ await execa(`npm`, ['run', 'build'], {
   cwd: join(root, 'packages', 'plugin-tsconfig'),
 })
 
-await rm(dist, { recursive: true, force: true })
+await rm(tmp, { recursive: true, force: true })
 await mkdir(dist, { recursive: true })
 
 const version = await getVersion()
@@ -78,3 +80,20 @@ const indexPath = join(dist, 'index.js')
 const indexContent = await readFile(indexPath, 'utf8')
 const newIndexContent = indexContent.replace('../plugin-tsconfig/dist/index.js', './rules.js')
 await writeFile(indexPath, newIndexContent)
+
+for (const packageName of ['plugin-github-actions', 'plugin-tsconfig']) {
+  const packageJson = await readJson(join(root, 'packages', packageName, 'package.json'))
+  delete packageJson.scripts
+  delete packageJson.devDependencies
+  delete packageJson.prettier
+  delete packageJson.jest
+  packageJson.version = version
+  packageJson.main = 'dist/index.js'
+  await mkdir(join(tmp, packageName))
+  await writeJson(join(tmp, packageName, 'package.json'), packageJson)
+
+  await bundleJs({
+    inFile: join(root, 'packages', packageName, 'src/index.ts'),
+    outFile: join(root, '.tmp', packageName, 'dist/index.js'),
+  })
+}
