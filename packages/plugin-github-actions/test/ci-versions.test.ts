87fb2dff71832139f138c942fa1972c6517484b5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-14 19:52:23 +0200

    feature: improve github actions rules (#159)

---
+++ packages/plugin-github-actions/test/ci-versions.test.ts
@@ -0,0 +1,62 @@
+import { RuleTester } from 'eslint'
+import parser from 'yaml-eslint-parser'
+import * as rule from '../src/ci-versions.ts'
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parser,
+    ecmaVersion: 2020,
+  },
+})
+
+// @ts-ignore
+ruleTester.run('unsupportedCiVersion', rule, {
+  valid: [
+    {
+      code: `
+jobs:
+  ci:
+    strategy:
+      matrix:
+        os: [ubuntu-24.04, macos-15, windows-2025]
+`,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+jobs:
+  ci:
+    strategy:
+      matrix:
+        os: [ubuntu-20.04, macos-15, windows-2025]`,
+      errors: [
+        {
+          messageId: 'unsupportedCiVersion',
+          line: 6,
+          column: 14,
+          endLine: 6,
+          endColumn: 26,
+        },
+      ],
+    },
+    {
+      code: `
+jobs:
+  ci:
+    strategy:
+      matrix:
+        os: [ubuntu-24.04, macos-12, windows-2025]`,
+
+      errors: [
+        {
+          messageId: 'unsupportedCiVersion',
+          line: 6,
+          column: 28,
+          endLine: 6,
+          endColumn: 36,
+        },
+      ],
+    },
+  ],
+})
