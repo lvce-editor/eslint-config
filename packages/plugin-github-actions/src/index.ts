29affef383e6f24992b51a962b8cece7f89e183b
Le Vivilet <levivlet@pm.me>
2025-05-14 19:55:20 +0200

    feature: remove ununsed build script

--- packages/plugin-github-actions/src/index.ts
+++ packages/plugin-github-actions/src/index.ts
@@ -1,15 +1,12 @@
-import eslintPluginYml from 'eslint-plugin-yml'
+import * as ciVersions from './ci-versions.ts'
 
-export const plugin = {
+const plugin = {
   meta: {
     name: 'github-actions',
     version: '0.0.1',
   },
   rules: {
-    ...eslintPluginYml.rules,
-  },
-  configs: {
-    recommended: eslintPluginYml.configs['flat/recommended'],
+    'ci-versions': ciVersions,
   },
 }
 
