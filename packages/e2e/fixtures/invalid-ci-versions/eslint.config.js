6fe630805008ce4a17605f165831d96566039d4c
Le Vivilet <levivilet@pm.me>
2025-11-04 20:20:10 +0100

    lint

--- packages/e2e/fixtures/invalid-ci-versions/eslint.config.js
+++ packages/e2e/fixtures/invalid-ci-versions/eslint.config.js
@@ -1,13 +1,3 @@
 import * as config from '../../../plugin-github-actions/src/index.ts'
-import eslintPluginYml from 'eslint-plugin-yml'
 
-export default [
-  ...eslintPluginYml.configs['flat/recommended'],
-  ...config.default,
-  {
-    files: ['src/*.yml'],
-    rules: {
-      'github-actions/ci-versions': 'error',
-    },
-  },
-]
+export default [...config.default]
