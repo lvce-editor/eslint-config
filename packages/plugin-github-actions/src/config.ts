f04b4a89caf4c72106cf0ca0ae02a37349ca7dd7
Le Vivilet <levivilet@pm.me>
2025-11-04 19:05:04 +0100

    feature: simplify github actions plugin config (#264)

---
+++ packages/plugin-github-actions/src/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+  ubuntu: ['ubuntu-24.04'],
+  macos: ['macos-15', 'macos-26'],
+  windows: ['windows-2025'],
+}
+
+export const platforms = {
+  windows: 'windows',
+  ubuntu: 'ubuntu',
+  macos: 'macos',
+}
