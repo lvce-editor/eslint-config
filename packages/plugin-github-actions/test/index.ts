4c3a6e2538f36df3d6624bb3d6a127a546351f47
Le Vivilet <levivilet@pm.me>
2025-11-04 23:29:49 +0100

    sort

--- packages/plugin-github-actions/test/index.ts
+++ packages/plugin-github-actions/test/index.ts
@@ -1,4 +1,4 @@
-import './ci-versions.test.ts'
 import './action-versions.test.ts'
-import './timeout-minutes.test.ts'
+import './ci-versions.test.ts'
 import './npm-registry.test.ts'
+import './timeout-minutes.test.ts'
