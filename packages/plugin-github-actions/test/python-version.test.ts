d4185c253cf391f9bde29c1c3f3080819dd39078
Le Vivilet <levivilet@pm.me>
2025-11-14 22:55:31 +0100

    feature: add python version rule

---
+++ packages/plugin-github-actions/test/python-version.test.ts
@@ -0,0 +1,48 @@
+import { RuleTester } from 'eslint'
+import parser from 'yaml-eslint-parser'
+import * as rule from '../src/rules/python-version.ts'
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parser,
+    ecmaVersion: 2020,
+  },
+})
+
+ruleTester.run('pythonVersion', rule, {
+  valid: [
+    {
+      code: `
+jobs:
+  publish-release:
+    runs-on: ubuntu-24.04
+    steps:
+      - name: Use Python 3.13
+        uses: actions/setup-python@5
+        with:
+          python-version: '3.13'`,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+jobs:
+  publish-release:
+    runs-on: ubuntu-24.04
+    steps:
+      - name: Use Python 3.13
+        uses: actions/setup-python@5
+        with:
+          python-version: '3.13'`,
+      errors: [
+        {
+          messageId: 'unsupportedPythonVersion',
+          column: 11,
+          endColumn: 33,
+          endLine: 8,
+          line: 8,
+        },
+      ],
+    },
+  ],
+})
