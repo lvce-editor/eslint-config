766074cc244148ae818d8d47f70397d84e050582
Le Vivilet <levivilet@pm.me>
2025-11-16 23:53:41 +0100

    fix

--- packages/plugin-github-actions/test/release-action.test.ts
+++ packages/plugin-github-actions/test/release-action.test.ts
@@ -37,7 +37,7 @@ jobs:
     steps:
       - name: Create GitHub release
         id: release
-        uses: softprops/action-gh-release@v2
+        uses: actions/create-release@v1
         env:
           GITHUB_TOKEN: \${{ secrets.GITHUB_TOKEN }}
         with:
@@ -60,7 +60,7 @@ jobs:
     steps:
       - name: Create GitHub release
         id: release
-        uses: actions/create-release@v1
+        uses: softprops/action-gh-release@v2
         env:
           GITHUB_TOKEN: \${{ secrets.GITHUB_TOKEN }}
         with:
