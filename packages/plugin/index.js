ad7d15a56904f3275f254a086908ecd58a8d7d50
Le Vivilet <levivilet@pm.me>
2026-02-05 17:05:15 +0100

    feature: add 'Consolas' to spellcheck custom words (#409)

--- packages/plugin/index.js
+++ packages/plugin/index.js
@@ -36,6 +36,7 @@ const defaultConfig = tseslint.config(
           cspell: {
             words: [
               'mixtral',
+              'Consolas',
               'testuser',
               'deepseek',
               'lvce',
