18182d99f78507ce24b740a46895973d4b297725
Le Vivilet <levivilet@pm.me>
2026-02-05 17:06:25 +0100

    feature: add 'navigations' and 'Overtype' to default configuration options

--- packages/plugin/index.js
+++ packages/plugin/index.js
@@ -112,6 +112,8 @@ const defaultConfig = tseslint.config(
               'pointercapture',
               'sortsize',
               'Icontest',
+              'navigations',
+              'Overtype',
             ],
           },
         },
