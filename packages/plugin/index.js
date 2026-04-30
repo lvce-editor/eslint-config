a30d9cd1e4afd11be1d92beaa3ab5bfc3579b9bc
Le Vivilet <levivilet@pm.me>
2026-04-30 17:37:37 +0200

    feature: disable additional sonarjs rules for improved linting flexibility

--- packages/plugin/index.js
+++ packages/plugin/index.js
@@ -319,6 +319,9 @@ const defaultConfig = tseslint.config(
       'sonarjs/pseudo-random': 'off',
       'sonarjs/no-invariant-returns': 'off',
       'sonarjs/no-small-switch': 'off',
+      'sonarjs/no-all-duplicated-branches': 'off',
+      'sonarjs/no-globals-shadowing': 'off',
+      'sonarjs/slow-regex': 'off',
     },
   },
   {
