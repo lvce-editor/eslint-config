16b900187c58532dc460b0f31ae5279f174799e4
Le Vivilet <levivilet@pm.me>
2026-06-16 12:30:01 +0200

    fix: remove unnecessary return statement in create function for valid-version rule

--- packages/plugin-nvmrc/src/rules/valid-version.ts
+++ packages/plugin-nvmrc/src/rules/valid-version.ts
@@ -132,7 +132,6 @@ export const create = (context: Rule.RuleContext): Rule.RuleListener => {
           messageId: 'versionTooNew',
           node,
         })
-        return
       }
     },
   }
