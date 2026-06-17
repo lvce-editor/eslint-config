1907ee39f97f46b9e00aadccdbb8216ed45bdbeb
Le Vivilet <levivilet@pm.me>
2026-06-17 10:17:29 +0200

    fix: disable 'unicorn/consistent-boolean-name' rule in default config (#557)

--- packages/plugin/index.js
+++ packages/plugin/index.js
@@ -444,6 +444,7 @@ const defaultConfig = tseslint.config(
       'unicorn/prefer-single-call': 'off',
       'unicorn/no-nested-ternary': 'off',
       'unicorn/prefer-short-arrow-method': 'off',
+      'unicorn/consistent-boolean-name': 'off',
     },
   },
   {
