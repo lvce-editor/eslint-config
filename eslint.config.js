cccbd065e65f153679fb9c06e7a1fe9a713d5e3d
Le Vivilet <levivilet@pm.me>
2026-06-17 11:14:55 +0200

    feature: add extra lint plugin (#561)

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,14 @@
 import config from './packages/plugin/index.js'
+import eslintPlugin from 'eslint-plugin-eslint-plugin'
 
-export default config
+export default [
+  ...config,
+  {
+    files: ['packages/plugin-*/src/rules/**/*.ts'],
+    ...eslintPlugin.configs['rules-recommended'],
+  },
+  {
+    files: ['packages/plugin-*/test/**/*.test.ts'],
+    ...eslintPlugin.configs['tests-recommended'],
+  },
+]
