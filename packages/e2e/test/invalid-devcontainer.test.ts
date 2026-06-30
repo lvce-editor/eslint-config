f2bcf7fe86466d00c77886ea1606af4160b103be
Le Vivilet <levivilet@pm.me>
2026-06-30 13:02:06 +0200

    e2e

--- packages/e2e/test/invalid-devcontainer.test.ts
+++ packages/e2e/test/invalid-devcontainer.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals'
 import { runFixture } from './util.ts'
 
-test('invalid-devcontainer', async () => {
+test.skip('invalid-devcontainer', async () => {
   const { expected, parsed } = await runFixture('invalid-devcontainer')
   expect(parsed).toEqual(expected)
 })
