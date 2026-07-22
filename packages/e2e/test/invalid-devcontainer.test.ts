48356d9356f5d153249441269773feb03b36d918
Le Vivilet <levivilet@pm.me>
2026-07-22 02:11:33 +0200

    chore: update eslint and eslint config to latest (#607)

--- packages/e2e/test/invalid-devcontainer.test.ts
+++ packages/e2e/test/invalid-devcontainer.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@jest/globals'
 import { runFixture } from './util.ts'
 
+// eslint-disable-next-line jest/no-disabled-tests -- The fixture expectations are currently out of sync.
 test.skip('invalid-devcontainer', async () => {
   const { expected, parsed } = await runFixture('invalid-devcontainer')
   expect(parsed).toEqual(expected)
