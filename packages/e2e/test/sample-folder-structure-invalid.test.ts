1466fcffaa4653e1b75409e7695129272630fb3f
Le Vivilet <levivilet@pm.me>
2026-07-22 00:05:30 +0000

    chore: document skipped eslint fixtures

--- packages/e2e/test/sample-folder-structure-invalid.test.ts
+++ packages/e2e/test/sample-folder-structure-invalid.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@jest/globals'
 import { runFixture } from './util.ts'
 
+// eslint-disable-next-line jest/no-disabled-tests -- The fixture does not currently produce a result file.
 test.skip('sample-folder-structure-invalid', async () => {
   const { expected, parsed } = await runFixture('sample-folder-structure-invalid')
   expect(parsed).toEqual(expected)
--- packages/e2e/test/sample-package-json-order.test.ts
+++ packages/e2e/test/sample-package-json-order.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@jest/globals'
 import { runFixture } from './util.ts'
 
+// eslint-disable-next-line jest/no-disabled-tests -- The fixture expectations are currently out of sync.
 test.skip('sample-package-json-order', async () => {
   const { expected, parsed } = await runFixture('sample-package-json-order')
   expect(parsed).toEqual(expected)
